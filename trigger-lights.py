345cbc0fef43fc8270ad0f0db341f7e2a3103fba
JettaPalisade <91229826+JettaPalisade@users.noreply.github.com>
2022-05-30 09:18:20 -0700

    Create trigger-lights.py
    subscribes to MQTT topic and calls arduino to trigger lights

---
+++ trigger-lights.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import serial
+import time
+import paho.mqtt.client as mqtt
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("prairie")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+    if msg.payload == "portal1 forward":
+        sendArduino(b"forward\n")
+    elif msg.payload == "portal1 backward":
+        sendArduino(b"backward\n")
+
+def sendArduino(msg):
+    print("sending to arduino: "+msg)
+    ser.write(msg)
+
+client = mqtt.Client("rasp1")
+client.username_pw_set("moonlight","collective")
+client.connect(host="73.254.192.189",port=41799)
+
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+ser.reset_input_buffer()
+
+client.loop_forever()
