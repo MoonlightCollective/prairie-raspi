29ee2e14dde7a1809e7d84766c69864829418e44
James Gwertzman <gwertzman@gmail.com>
2022-05-30 16:57:06 -0700

    Fixed minor bugs

--- trigger-lights.py
+++ trigger-lights.py
@@ -14,20 +14,23 @@ def on_connect(client, userdata, flags, rc):
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
-    if msg.payload == "portal1 forward":
-        sendArduino(b"forward\n")
-    elif msg.payload == "portal1 backward":
-        sendArduino(b"backward\n")
+    if msg.payload == b"portal1 forward":
+        sendArduino("forward\n")
+    elif msg.payload == b"portal1 backward":
+        sendArduino("backward\n")
 
 def sendArduino(msg):
     print("sending to arduino: "+msg)
-    ser.write(msg)
+    ser.write(msg.encode('utf-8'))
 
-client = mqtt.Client("rasp1")
+client = mqtt.Client("rasp2")
 client.username_pw_set("moonlight","collective")
+client.on_connect = on_connect
+client.on_message = on_message
 client.connect(host="73.254.192.189",port=41799)
 
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 ser.reset_input_buffer()
 
-client.loop_forever()
+while True:
+  client.loop()
