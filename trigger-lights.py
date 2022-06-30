8b6935557ebbe3fd371e6d96ef5b8d23c8bc83cf
James Gwertzman <gwertzman@gmail.com>
2022-06-30 00:14:09 -0700

    updated trigger lights to monitor sensors and play sequence

--- trigger-lights.py
+++ trigger-lights.py
@@ -2,6 +2,8 @@
 import serial
 import time
 import paho.mqtt.client as mqtt
+import automationhat
+import pygame
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -14,25 +16,62 @@ def on_connect(client, userdata, flags, rc):
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
-    if msg.payload == b"portal1 forward":
-        sendArduino("forward\n")
-    elif msg.payload == b"portal1 backward":
-        sendArduino("backward\n")
 
 def sendArduino(msg):
     print("sending to arduino: "+msg)
     ser.write(msg.encode('utf-8'))
 
-client = mqtt.Client("rasp2")
+client = mqtt.Client("rasp")
 client.username_pw_set("moonlight","collective")
 client.on_connect = on_connect
 client.on_message = on_message
 client.connect(host="73.254.192.189",port=41799)
-client.on_message = on_message
-client.on_connect = on_connect
 
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 ser.reset_input_buffer()
 
+if automationhat.is_automation_hat():
+    automationhat.light.power.write(1)
+
+pygame.mixer.init()
+pygame.mixer.music.load("transition.wav")
+
+# we have three states... CLEAR,FORWARD,BACKWARD
+# 0 = the portal is empty, no one is walking
+# 1 = someone has entered the portal going forward
+# 2 = someone has entered the portal going backward
+
+# when reading the inputs, 1 = no activity (pulled high by default); 0 = path is broken (triggered low)
+
+state = 0
+last = time.time()
+
 while True:
-  client.loop()
+    client.loop()
+
+    if time.time() - last > 30:
+      # send keep alive every 30 seconds
+      last = time.time()
+      client.publish ("prairie","portal1 alive")
+
+    if state == 0:
+      if automationhat.input[0].read() == 0:
+        # someone is going forward
+        client.publish ("prairie","portal1 forward") 
+        print ("forward")
+        sendArduino("forward")
+        pygame.mixer.music.play()
+        state = 1
+        time.sleep(0.5) # add a tiny delay to avoid retriggering too often
+      #elif automationhat.input[1].read() == 0:
+      #  client.publish ("prairie","portal1 backward") 
+      #  print ("backward")
+      #  state = 2  
+      #  time.sleep(0.5) # add a tiny delay to avoid retriggering too often
+      else:
+        time.sleep(0.1)
+    if state == 1 or state == 2:
+      if automationhat.input[0].read() == 1:  # and automationhat.input[1].read() == 1:
+        state = 0
+      else:
+        time.sleep(0.1)
