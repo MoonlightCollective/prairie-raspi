4a912a9e04afd6013a8ed6a7c3e70b6ddc0f10c0
James Gwertzman <gwertzman@gmail.com>
2022-07-28 22:36:40 -0700

    swapped Portal to portal for mqtt topic

--- portal.py
+++ portal.py
@@ -16,7 +16,7 @@ def on_connect(client, userdata, flags, rc):
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("Portal")
+    client.subscribe("portal")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
