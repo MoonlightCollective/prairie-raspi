0d332e1137b45231c9b557d53bcf838536156d6c
Nathan Pegram <nathan.pegram@gmail.com>
2022-08-02 12:47:11 -0700

    add force option to update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -11,49 +11,44 @@ git checkout .
 git pull
 
 
-cd ~/prairie-raspi/arduino
+if [[ git diff-index --name-only HEAD | grep -e 'portal1.ino' -e `readlink portal1.ino` ]] || [[ $1 == "force" ]] ; then
 
-if [[ "$HOSTNAME" == "portal1" ]]; then
-    if git diff-index --name-only HEAD | grep -e 'portal1.ino' -e `readlink portal1.ino`; then
-        echo "Updating portal 1 arduino"
-        arduino-cli compile --fqbn adafruit:samd:adafruit_feather_m4 portal1.ino
-        arduino-cli upload -p /dev/ttyACM0 --fqbn adafruit:samd:adafruit_feather_m4 portal1.ino
-    fi
-fi
-if [[ "$HOSTNAME" == "portal2" ]]; then
-    if git diff-index --name-only HEAD | grep -e 'portal2.ino' -e `readlink portal2.ino`; then
-        echo "Updating portal 2 arduino"
-        arduino-cli compile --fqbn adafruit:samd:adafruit_feather_m4 portal2.ino
-        arduino-cli upload -p /dev/ttyACM0 --fqbn adafruit:samd:adafruit_feather_m4 portal2.ino
-    fi
-fi
-if [[ "$HOSTNAME" == "portal3" ]]; then
-    if git diff-index --name-only HEAD | grep -e 'portal3.ino' -e `readlink portal3.ino`; then
-        echo "Updating portal 3 arduino"
-        arduino-cli compile --fqbn aadafruit:samd:adafruit_itsybitsy_m4  portal3.ino
-        arduino-cli upload -p /dev/ttyACM0 --fqbn adafruit:samd:adafruit_itsybitsy_m4  portal3.ino
-    fi
-fi
-if [[ "$HOSTNAME" == "portal4" ]]; then
-    if git diff-index --name-only HEAD | grep -e 'portal4.ino' -e `readlink portal4.ino`; then
-        echo "Updating portal 4 arduino"
-        arduino-cli compile --fqbn adafruit:samd:adafruit_feather_m4 portal4.ino
-        arduino-cli upload -p /dev/ttyACM0 --fqbn adafruit:samd:adafruit_feather_m4 portal4.ino
-    fi
-fi
+    cd ~/prairie-raspi/arduino
 
-if [[ "$HOSTNAME" == "booth1" ]]; then
-    if git diff-index --name-only HEAD | grep -e 'booth1.ino' -e `readlink booth1.ino`; then
-        echo "Updating booth 1 arduino"
-        arduino-cli compile --fqbn arduino:avr:nano booth1.ino
-        arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:nano booth1.ino
+    if [[ "$HOSTNAME" == "portal1" ]]; then
+            echo "Updating portal 1 arduino"
+            arduino-cli compile --fqbn adafruit:samd:adafruit_feather_m4 portal1.ino
+            arduino-cli upload -p /dev/ttyACM0 --fqbn adafruit:samd:adafruit_feather_m4 portal1.ino
     fi
-fi
-if [[ "$HOSTNAME" == "booth2" ]]; then
-    if git diff-index --name-only HEAD | grep -e 'booth2.ino' -e `readlink booth2.ino`; then
-        echo "Updating booth 2 arduino"
-        arduino-cli compile --fqbn arduino:avr:nano booth2.ino
-        arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:nano booth2.ino
+
+    if [[ "$HOSTNAME" == "portal2" ]]; then
+            echo "Updating portal 2 arduino"
+            arduino-cli compile --fqbn adafruit:samd:adafruit_feather_m4 portal2.ino
+            arduino-cli upload -p /dev/ttyACM0 --fqbn adafruit:samd:adafruit_feather_m4 portal2.ino
+    fi
+
+    if [[ "$HOSTNAME" == "portal3" ]]; then
+            echo "Updating portal 3 arduino"
+            arduino-cli compile --fqbn aadafruit:samd:adafruit_itsybitsy_m4  portal3.ino
+            arduino-cli upload -p /dev/ttyACM0 --fqbn adafruit:samd:adafruit_itsybitsy_m4  portal3.ino
+    fi
+
+    if [[ "$HOSTNAME" == "portal4" ]]; then
+            echo "Updating portal 4 arduino"
+            arduino-cli compile --fqbn adafruit:samd:adafruit_feather_m4 portal4.ino
+            arduino-cli upload -p /dev/ttyACM0 --fqbn adafruit:samd:adafruit_feather_m4 portal4.ino
+    fi
+
+    if [[ "$HOSTNAME" == "booth1" ]]; then
+            echo "Updating booth 1 arduino"
+            arduino-cli compile --fqbn arduino:avr:nano booth1.ino
+            arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:nano booth1.ino
+    fi
+
+    if [[ "$HOSTNAME" == "booth2" ]]; then
+            echo "Updating booth 2 arduino"
+            arduino-cli compile --fqbn arduino:avr:nano booth2.ino
+            arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:nano booth2.ino
     fi
 fi
 
