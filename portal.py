39ce7351a2f4453007a91714eca6dbae1cc21cd0
James Gwertzman <gwertzman@gmail.com>
2022-08-27 21:52:48 -0700

    set up mqtt client to try connecting every 30 seconds

--- portal.py
+++ portal.py
@@ -13,6 +13,7 @@ import math
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
+    mqtt_connection = True
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
@@ -58,12 +59,7 @@ whisperSnd.play(-1)
 client = mqtt.Client(socket.gethostname())
 client.on_connect = on_connect
 client.on_message = on_message
-try:
-  client.connect(host="192.168.0.202")
-  client.loop_start()
-except Exception as e:
-  print (e)
-  pass
+mqtt_connection = False
 
 ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
 ser.reset_input_buffer()
@@ -78,6 +74,14 @@ while True:
     if time.time() - last > 30:
       # send keep alive every 30 seconds
       last = time.time()
+
+      if mqtt_connection is False:
+        try:
+          client.connect(host="192.168.0.202")
+          client.loop_start()
+        except Exception as e:
+          print (e)
+
       fieldDict = { "sender":socket.gethostname() }
       tagsDict = { "host":socket.gethostname() }
       msgDict = { "name":"keep-alive", "fields":fieldDict, "tags":tagsDict, "timestamp":math.floor(time.time()) }
